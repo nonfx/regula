798fc48fca21df7db07eff5f7064d2725f410051
cc-shekher <shekhertrvdd@gmail.com>
2024-11-19 16:46:34 +0530

    added regula scan util

---
+++ cmd/regula_scan.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/fugue/regula/v3/pkg/loader"
+	"github.com/fugue/regula/v3/pkg/rego"
+	"github.com/fugue/regula/v3/pkg/reporter"
+)
+
+var iacPath = []string{"/Users/chandrashekhar/source-code/src/github.com/nonfx/tf-regula-test/module-test/iac"}
+var pacPath = []string{"/Users/chandrashekhar/source-code/src/github.com/nonfx/stance/foundation/data/rules/aws"}
+
+func RunRegulaScan(args []string) error {
+	// CLI-arg-only options
+	noConfig := true
+
+	// Config-file specific options
+	var rootDir string
+	var configPath string
+
+	// Inputs
+	inputs := iacPath
+	includes := pacPath
+
+	// Enum types
+
+	inputTypes := []loader.InputType{loader.Tf}
+	format := reporter.JSON
+
+	config := &runConfig{
+		configPath: configPath,
+		format:     format,
+		includes:   includes,
+		inputs:     inputs,
+		inputTypes: inputTypes,
+		noBuiltIns: true,
+		noConfig:   noConfig,
+		rootDir:    rootDir,
+	}
+	if err := config.Validate(); err != nil {
+		return err
+	}
+
+	// Interpret configuration
+	configLoader, err := config.ConfigurationLoader()
+	if err != nil {
+		return err
+	}
+	providers, err := config.Providers()
+	if err != nil {
+		return err
+	}
+	resultProcessor := config.ResultProcessor()
+
+	// Execution
+	ctx := context.Background()
+	loadedConfigs, err := configLoader()
+	if err != nil {
+		return err
+	}
+	result, err := rego.RunRules(ctx, &rego.RunRulesOptions{
+		Providers: providers,
+		Input:     loadedConfigs.RegulaInput(),
+		Query:     config.RunRulesQuery(),
+	})
+	if err != nil {
+		return err
+	}
+	if err := resultProcessor(ctx, loadedConfigs, result); err != nil {
+		return err
+	}
+	return nil
+}
